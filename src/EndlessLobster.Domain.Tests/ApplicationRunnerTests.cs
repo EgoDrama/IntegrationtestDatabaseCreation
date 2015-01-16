94be328ad2d3dc873b7c2d7cfc5a75605fd484ab
EgoDrama <it_david@hotmail.com>
2015-01-16 13:02:36 +0100

    added sdf database

--- src/EndlessLobster.Domain.Tests/ApplicationRunnerTests.cs
+++ src/EndlessLobster.Domain.Tests/ApplicationRunnerTests.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlServerCe;
+using System.IO;
+using System.Reflection;
 using Castle.Windsor;
 using EndlessLobster.Application;
 using EndlessLobster.Domain.Repository;
@@ -13,12 +16,30 @@ namespace EndlessLobster.Domain.Tests
     public class ApplicationRunnerTests
     {
         private IWindsorContainer _container;
+        private IDatabaseFactory _databaseFactory;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _databaseFactory = new TestDatabaseFactory();
+
+            using (var sqlCeEngine = new SqlCeEngine(_databaseFactory.GetConnection().ConnectionString))
+            {
+                var databasePath = GetDatabasePath();
+
+                if (!File.Exists(databasePath))
+                {
+                    sqlCeEngine.CreateDatabase();
+                }
+            }
+
+            ExecuteDatabaseScript("create_database.txt");
+        }
 
         [SetUp]
         public void SetUp()
         {
-            var databaseFactory = new DatabaseFactory();
-            var bootstrapper = new Bootstrapper(databaseFactory);
+            var bootstrapper = new Bootstrapper(_databaseFactory);
             
             using (var container = new WindsorContainer())
             {
@@ -35,7 +56,73 @@ namespace EndlessLobster.Domain.Tests
 
             var actual = artistModifier.ModifyArtist("foo");
 
-            actual.Name.Should().Be("AC/DCfoo");
+            actual.Name.Should().Be("Foo Barfoo");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ExecuteDatabaseScript("delete_database.txt");
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            var databasePath = GetDatabasePath();
+
+            if (File.Exists(databasePath))
+            {
+                File.Delete(databasePath);
+            }
+        }
+
+        private string GetDatabasePath()
+        {
+            const string databaseName = "TestDatabase.sdf";
+            var executingAssembly = GetPathExecutingAssembly();
+            var databasePath = Path.Combine(executingAssembly, "Data");
+            return string.Format(databaseName, databasePath);
+        }
+
+        private static string GetPathExecutingAssembly()
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+
+        private void ExecuteDatabaseScript(string fileName)
+        {
+            using (var connection = _databaseFactory.GetConnection())
+            {
+                connection.Open();
+                ExecuteDatabaseScript(connection, fileName);
+            }
+        }
+
+        private static void ExecuteDatabaseScript(IDbConnection connection, string fileName)
+        {
+            var databasePath = GetPathExecutingAssembly();
+            var file = Path.Combine(databasePath, "Data", fileName);
+
+            var sql = ReadSqlFromFile(file);
+            var sqlCmds = sql.Split(new[] { "GO" }, int.MaxValue, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sqlCmd in sqlCmds)
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = sqlCmd;
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private static string ReadSqlFromFile(string sqlFilePath)
+        {
+            using (TextReader r = new StreamReader(sqlFilePath))
+            {
+                return r.ReadToEnd();
+            }
         }
     }
 
