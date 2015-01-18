3482c3ff6262d2df76f62ae0107b0ab795bf3cfb
EgoDrama <it_david@hotmail.com>
2015-01-18 07:50:44 +0100

    added unit test for artist modifier

--- src/EndlessLobster.Domain.Tests/ApplicationRunnerTests.cs
+++ src/EndlessLobster.Domain.Tests/ApplicationRunnerTests.cs
@@ -4,6 +4,7 @@ using Castle.Windsor;
 using EndlessLobster.Application;
 using EndlessLobster.Domain.Repository;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 
 namespace EndlessLobster.Domain.Tests
@@ -50,6 +51,20 @@ namespace EndlessLobster.Domain.Tests
 
         [Test]
         public void Returns_Modified_Artist()
+        {
+            var artistRepository = new Mock<IRepository<Artist>>();
+            var artist = new Artist { Name = "Foo" };
+            artistRepository.Setup(x => x.Get(It.IsAny<int>())).Returns(artist);
+
+            var artistModifier = new ArtistModifier(artistRepository.Object);
+
+            var actual = artistModifier.ModifyArtist("bar");
+
+            actual.Name.Should().Be("Foobar");
+        }
+
+        [Test]
+        public void Returns_Modified_Artist_Integration()
         {
             var artistRepository = _container.Resolve<IRepository<Artist>>();
             var artistModifier = new ArtistModifier(artistRepository);
