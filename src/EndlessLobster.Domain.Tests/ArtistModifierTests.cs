061e3b57d567552819868baf3c104cb2a98d436d
EgoDrama <it_david@hotmail.com>
2015-01-22 13:36:07 +0100

    demo done

--- src/EndlessLobster.Domain.Tests/ArtistModifierTests.cs
+++ src/EndlessLobster.Domain.Tests/ArtistModifierTests.cs
@@ -1,4 +1,8 @@
-﻿using EndlessLobster.Domain.Repository;
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.IO;
+using EndlessLobster.Domain.Repository;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
@@ -8,6 +12,46 @@ namespace EndlessLobster.Domain.Tests
     [TestFixture]
     public class ArtistModifierTests
     {
+        private IDatabaseFactory _databaseFactory;
+        private DatabaseHelper _databaseHelper;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _databaseFactory = new FakeDatabaseFactory();
+            _databaseHelper = new DatabaseHelper(_databaseFactory);
+            
+            using (var connection = new SqlCeEngine(_databaseFactory.GetConnection().ConnectionString))
+            {
+                if (!File.Exists(_databaseHelper.GetDatabasePath()))
+                {
+                    connection.CreateDatabase();    
+                }
+            }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _databaseHelper.ExecuteDatabaseScript("create_database.txt");
+            _databaseHelper.ExecuteDatabaseScript("populate_database.txt");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _databaseHelper.ExecuteDatabaseScript("delete_database.txt");
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            if (File.Exists(_databaseHelper.GetDatabasePath()))
+            {
+                File.Delete(_databaseHelper.GetDatabasePath());
+            }
+        }
+
         [Test]
         public void Returns_Modified_ArtistName()
         {
@@ -26,13 +70,22 @@ namespace EndlessLobster.Domain.Tests
         [Test]
         public void Returns_Modified_ArtistName_Integration()
         {
-            var artistRepository = new ArtistRepository();
+            
+            var artistRepository = new ArtistRepository(_databaseFactory);
             var artistModifier = new ArtistModifier(artistRepository);
             const int artistId = 1;
 
             var actual = artistModifier.ModifyArtistName(artistId, "foo");
 
-            actual.Name.Should().Be("AC/DC - foo");
+            actual.Name.Should().Be("Foo Bar - foo");
+        }
+    }
+
+    public class FakeDatabaseFactory : IDatabaseFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            return new SqlCeConnection(ConfigurationManager.ConnectionStrings["ChinookStore"].ConnectionString);
         }
     }
 }
